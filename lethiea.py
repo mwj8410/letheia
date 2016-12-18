89eb672ba206660f11d2d779f3ac5f23ab99d08f
Matthew Jackson <mwj8410@gmail.com>
2016-12-18 17:45:28 +0000

    feat(decode): decode encoded file message to file

--- lethiea.py
+++ lethiea.py
@@ -1,4 +1,5 @@
 import random
+import re
 import sys
 
 DECODE_FILE_NAME = 'decoded.txt'
@@ -10,6 +11,13 @@ KEY_SIZE_MIN = 32
 KEY_WORD_RATIO = 2
 
 CODE_CHARACTER_SET = '0123456789ABCDEF'
+
+def decode_message(code, key, dictionary):
+    decode_partial = code
+    for i in key:
+        decode_partial = decode_partial.replace(i[1], '[' + get_word_by_index(int(i[0]), dictionary) + ']')
+    word = re.compile('\[[a-z\s]*.\]')
+    return normalize_message(' '.join(word.findall(decode_partial)))
 
 def encode_message(message, key, dictionary):
     message_parts = str.split(message, ' ')
@@ -32,6 +40,9 @@ def generate_lookup_table(word_set, codebook_name):
             code_value = ''.join([random.choice(CODE_CHARACTER_SET) for x in range(key_size)])
             lookup_table_file.write(str(index) + ':' + code_value + '\n')
     lookup_table_file.close()
+
+def get_word_by_index(index, dictionary):
+    return dictionary[index]
 
 def normalize_message(message):
     charset = 'abcdefghijklmnopqrstuvwxyz '
@@ -56,6 +67,12 @@ def read_dictionary():
         output.append(j)
     return output
 
+def read_encoded_file(file_name):
+    dictionary_file = open(file_name, 'r')
+    content = dictionary_file.readlines()[0]
+    dictionary_file.close()
+    return content
+
 def read_lookup_table(filename):
     file_contents = open(filename + '.tbl', 'r').readlines()
     output = []
@@ -64,11 +81,19 @@ def read_lookup_table(filename):
         output.append(str.split(j, ':'))
     return output
 
+def save_decoded_message(message_content):
+    message_file = open(DECODE_FILE_NAME, 'w')
+    message_file.write(message_content)
+    message_file.close()
+
 def save_encoded_message(codes):
     message_file = open(ENCODED_FILE_NAME, 'w')
     message_file.write(''.join(codes))
     message_file.close()
 
+def trin_junk(content):
+    content
+    
 # lethiea
 if (len(sys.argv) < 3):
     print('Invalid number of arguments supplied.')
@@ -96,6 +121,12 @@ if (len(sys.argv) == 4 and sys.argv[2] == '-m'):
 # lethiea <code-book-name> -d <file-name>
 if (len(sys.argv) == 4 and sys.argv[2] == '-d'):
     print('decode indicated file')
+    lookup_table_file_name = sys.argv[1]
+    file_name = sys.argv[3]
+    # print read_encoded_file(file_name)
+    save_decoded_message(
+        decode_message(read_encoded_file(file_name), read_lookup_table(lookup_table_file_name), read_dictionary())
+    )
     sys.exit(0)
 
 print(sys.argv)
